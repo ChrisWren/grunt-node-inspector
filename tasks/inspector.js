8576bcd952d3df8e9e6d98f6157e903e2486971f
Tom Spencer <fiznool@gmail.com>
2015-10-02 10:06:51 +0100

    Options parsing
     - Fix `hidden` option
     - Changed to `preload` from `no-preload`
     - Added SSL options

--- tasks/inspector.js
+++ tasks/inspector.js
@@ -6,29 +6,38 @@
  * Licensed under the MIT license.
  */
 /*jshint node: true*/
+
 module.exports = function (grunt) {
   'use strict';
+  var util = require('util');
+
   grunt.registerMultiTask('node-inspector', 'Runs node-inspector to debug your node.js JavaScripts', function () {
     var options = this.options();
     var done = this.async();
     var args = [require.resolve('node-inspector/bin/inspector')];
+    var pushArg = function(option, val) {
+      args.push('--' + option);
+      args.push(val);
+    };
     [
-      'web-port',
-      'web-host',
       'debug-port',
+      'web-host',
+      'web-port',
       'save-live-edit',
-      'readTimeout',
+      'preload',
+      'hidden',
       'stack-trace-limit',
-      'no-preload',
-      'hidden'
+      'ssl-key',
+      'ssl-cert'
     ].forEach(function (option) {
-      if (option in options) {
-        args.push('--' + option);
-        if (option === 'hidden') {
-          args.push(JSON.stringify(options[option]));
-          return;
+      if(option in options) {
+        if(util.isArray(options[option])) {
+          options[option].forEach(function(val) {
+            pushArg(option, val);
+          });
+        } else {
+          pushArg(option, options[option]);
         }
-        args.push(options[option]);
       }
     });
 
